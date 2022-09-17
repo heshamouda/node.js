94e4a993117d7b78903aad44cb979b57f66ffe5b
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2022-09-17 13:23:18 +0200

    udate

--- 01_06/async-demo.js
+++ 01_06/async-demo.js
@@ -1,9 +1,9 @@
-fs = require('fs');
+fs = require("fs");
 
 function phoneNumber(err, data) {
-	console.log('data:', data);
+  console.log("data:", data);
 }
 
-fs.readdir('c:/', phoneNumber);
+fs.readdir("c:/", phoneNumber);
 
 console.log("this comes after");
